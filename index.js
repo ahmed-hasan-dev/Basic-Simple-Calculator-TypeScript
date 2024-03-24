da9699b3248ae63b24dd51378f1749d52e749906
Muhammad Ahmed Hasan <106650490+ahmed-hasan-dev@users.noreply.github.com>
2024-03-25 03:50:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform actions",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// console.log(answer);
+if (answer.operator === "Addition") {
+    console.log("Your answer is", answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log("Your answer is", answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log("Your answer is", answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log("Your answer is", answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please Enter Valid Operator");
+}
